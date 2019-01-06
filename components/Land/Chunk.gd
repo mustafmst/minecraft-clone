02fb3f32234a986eb78794270592173b0ab01875
Paweł Mstowski <pawel.mstowski@gmail.com>
2019-01-06 13:49:57 +0100

    sition of chunk and block placing

--- components/Land/Chunk.gd
+++ components/Land/Chunk.gd
@@ -1,10 +1,11 @@
-extends Node
+extends Spatial
 
 # Scenes and Objects
 const Block = preload("res://components/Block/Block.tscn")
 const Chunk = preload("res://components/Land/Chunk.tscn")
 
 # Variables
+const ChunkSize = 10
 var neighourChunks = [[null, null, null],
                       [null, null, null],
                       [null, null, null]]
@@ -12,18 +13,20 @@ var neighourChunks = [[null, null, null],
 # need to implement here chunk generation
 func generate_map():
     var map = []
-    for i in range(10):
+    for i in range(ChunkSize):
         var tmp = []
-        for j in range(10):
+        for j in range(ChunkSize):
             tmp.push_back(1)
         map.push_back(tmp)
     return map
 
 
 # Creating new Chunk
-static func create_new(landScene, parentChunk, parentX, parentY):
+static func create_new(landScene, parentChunk, parentX, parentY, pos):
     var newChunk = Chunk.instance()
-    newChunk.add_neighbour(parentChunk, parentX, parentY)
+    newChunk.translate(Vector3(pos.x, 0, pos.y))
+    if parentChunk != null:
+        newChunk.add_neighbour(parentChunk, parentX, parentY)
     landScene.add_child(newChunk)
     return newChunk
 
@@ -37,23 +40,26 @@ func _init():
 
 
 func _ready():
-    # Called when the node is added to the scene for the first time.
-    # Initialization here
+    self.add_neighbour(self,1,1)
     pass
 
 
 func put_blocks(pos, height):
+    var chunkPos = self.translation
     for h in range(height):
         var item = Block.instance()
-        item.translate(Vector3(pos.x, h, pos.y))
+        item.translate(Vector3((chunkPos.x*ChunkSize)+pos.x, h, (chunkPos.z*ChunkSize)+pos.y))
         add_child(item)
     pass
 
 
 # Helper methods
 func add_neighbour(chunk, x, y):
-    if(chunk != self && x != 1 && y != 1):
-        neighourChunks[x][y] = chunk
+    neighourChunks[x][y] = chunk
     pass
 
+
+func set_as_active():
+    
+    pass
 
