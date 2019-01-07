46dfdf6defe15b775297581fb0f6e5859852b158
Paweł Mstowski <pawel.mstowski@gmail.com>
2019-01-07 18:15:49 +0100

    Add big map generator
    Update Land.tscn and MapGenerator.gd

---
+++ components/Land/MapGenerator.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var mapSize = null
+
+var map = null
+
+func generate_map(size, maxHeight, minHeight, windowSize):
+    mapSize = size
+    var tmpMap = []
+    for i in range(size.x):
+        var mapLine = []
+        for j in range(size.y):
+             mapLine.append(randi()%(maxHeight-minHeight)+minHeight)
+        tmpMap.append(mapLine)
+    map = tmpMap
+    pass
+
+
+func get_height(point):
+    var x = int(point.x)+int(mapSize.x/2)
+    var y = int(point.y)+int(mapSize.y/2)
+    if x > -1 && x < mapSize.x && y > -1 && y < mapSize.y:
+        return map[x][y]
+    else:
+        return 0
+    pass
