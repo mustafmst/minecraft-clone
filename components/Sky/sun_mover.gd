d2fa6080c81766bd189c7344a96a728c92031a87
Maciej W <wisnia91@gmail.com>
2019-01-29 02:40:01 +0100

    Moon movement PoC

--- components/Sky/sun_mover.gd
+++ components/Sky/sun_mover.gd
@@ -5,23 +5,15 @@ const change_color_range = PI / 8
 
 onready var sun = get_node("sun")
 
-func _ready():
-    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
 
-    if Input.is_action_just_pressed("ui_cancel"):
-        if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-            Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-        else:
-            Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
     var new_sun_rotation = get_rotation()
     new_sun_rotation.z += (PI * speed) * delta
-    
+
     if(new_sun_rotation.z > 2 * PI):
         new_sun_rotation.z -= 2 * PI
     elif(new_sun_rotation.z < 0):
         new_sun_rotation.z += 2 * PI
-    
+
     set_rotation(new_sun_rotation)
