cb458292d74231adaa906b2b598913fd98bb084e
Maciej W <wisnia91@gmail.com>
2019-02-04 03:03:27 +0100

    Make sky act as real more

--- components/Sky/moon_mover.gd
+++ components/Sky/moon_mover.gd
@@ -1,14 +1,14 @@
 extends Position3D
 
+#var speed = 0.001388889 # 1h in game world = 1 minute in real
 var speed = 0.2
-const change_color_range = PI / 8
 
 onready var moon = get_node("moon")
 
 func _ready():
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _process(delta):
+func _physics_process(delta):
 
     if Input.is_action_just_pressed("ui_cancel"):
         if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
@@ -23,5 +23,10 @@ func _process(delta):
         new_moon_rotation.z -= 2 * PI
     elif(new_moon_rotation.z < 0):
         new_moon_rotation.z += 2 * PI
+
+    if ((new_moon_rotation.z > 0) && (new_moon_rotation.z < PI)):
+        moon.visible = true
+    else:
+        moon.visible = false
 
     set_rotation(new_moon_rotation)
--- components/Sky/sun_mover.gd
+++ components/Sky/sun_mover.gd
@@ -1,19 +1,26 @@
 extends Position3D
 
-var speed = 0.1
-const change_color_range = PI / 8
+#var speed = 0.001388889 # 1h in game world = 1 minute in real
+var speed = 0.2
 
 onready var sun = get_node("sun")
+onready var w_env = get_node("WorldEnvironment")
 
 
-func _process(delta):
-
+func _physics_process(delta):
     var new_sun_rotation = get_rotation()
     new_sun_rotation.z += (PI * speed) * delta
 
-    if(new_sun_rotation.z > 2 * PI):
+    if (new_sun_rotation.z > 2 * PI):
         new_sun_rotation.z -= 2 * PI
-    elif(new_sun_rotation.z < 0):
+    elif (new_sun_rotation.z < 0):
         new_sun_rotation.z += 2 * PI
+
+    if ((new_sun_rotation.z > 0) && (new_sun_rotation.z < PI)):
+        w_env.environment.background_energy = 0.25
+        sun.visible = false
+    else:
+        w_env.environment.background_energy = 0.75
+        sun.visible = true
 
     set_rotation(new_sun_rotation)
